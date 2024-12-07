8fd246139ed4d56ebccef15cf829dc6cd20b402b
Duc Anh TA <83185055+taducanh1605@users.noreply.github.com>
2024-12-07 21:55:29 +0100

    update new set of decks
    update new set of decks

---
+++ js/spread.js
@@ -0,0 +1,121 @@
+var set = {
+    "gothic": ["tarot_00.png", "tarot_01.png", "tarot_02.png", "tarot_03.png", "tarot_04.png", "tarot_05.png", "tarot_06.png", "tarot_07.png", "tarot_08.png", "tarot_09.png", "tarot_10.png", "tarot_11.png", "tarot_12.png", "tarot_13.png", "tarot_14.png", "tarot_15.png", "tarot_16.png", "tarot_17.png", "tarot_18.png", "tarot_19.png", "tarot_20.png", "tarot_21.png", "tarot_c01.png", "tarot_c02.png", "tarot_c03.png", "tarot_c04.png", "tarot_c05.png", "tarot_c06.png", "tarot_c07.png", "tarot_c08.png", "tarot_c09.png", "tarot_c10.png", "tarot_c11.png", "tarot_c12.png", "tarot_c13.png", "tarot_c14.png", "tarot_p01.png", "tarot_p02.png", "tarot_p03.png", "tarot_p04.png", "tarot_p05.png", "tarot_p06.png", "tarot_p07.png", "tarot_p08.png", "tarot_p09.png", "tarot_p10.png", "tarot_p11.png", "tarot_p12.png", "tarot_p13.png", "tarot_p14.png", "tarot_s01.png", "tarot_s02.png", "tarot_s03.png", "tarot_s04.png", "tarot_s05.png", "tarot_s06.png", "tarot_s07.png", "tarot_s08.png", "tarot_s09.png", "tarot_s10.png", "tarot_s11.png", "tarot_s12.png", "tarot_s13.png", "tarot_s14.png", "tarot_w01.png", "tarot_w02.png", "tarot_w03.png", "tarot_w04.png", "tarot_w05.png", "tarot_w06.png", "tarot_w07.png", "tarot_w08.png", "tarot_w09.png", "tarot_w10.png", "tarot_w11.png", "tarot_w12.png", "tarot_w13.png", "tarot_w14.png"],
+    "rider-waite": ["tarot_00.png", "tarot_01.png", "tarot_02.png", "tarot_03.png", "tarot_04.png", "tarot_05.png", "tarot_06.png", "tarot_07.png", "tarot_08.png", "tarot_09.png", "tarot_10.png", "tarot_11.png", "tarot_12.png", "tarot_13.png", "tarot_14.png", "tarot_15.png", "tarot_16.png", "tarot_17.png", "tarot_18.png", "tarot_19.png", "tarot_20.png", "tarot_21.png", "tarot_c01.png", "tarot_c02.png", "tarot_c03.png", "tarot_c04.png", "tarot_c05.png", "tarot_c06.png", "tarot_c07.png", "tarot_c08.png", "tarot_c09.png", "tarot_c10.png", "tarot_c11.png", "tarot_c12.png", "tarot_c13.png", "tarot_c14.png", "tarot_p01.png", "tarot_p02.png", "tarot_p03.png", "tarot_p04.png", "tarot_p05.png", "tarot_p06.png", "tarot_p07.png", "tarot_p08.png", "tarot_p09.png", "tarot_p10.png", "tarot_p11.png", "tarot_p12.png", "tarot_p13.png", "tarot_p14.png", "tarot_s01.png", "tarot_s02.png", "tarot_s03.png", "tarot_s04.png", "tarot_s05.png", "tarot_s06.png", "tarot_s07.png", "tarot_s08.png", "tarot_s09.png", "tarot_s10.png", "tarot_s11.png", "tarot_s12.png", "tarot_s13.png", "tarot_s14.png", "tarot_w01.png", "tarot_w02.png", "tarot_w03.png", "tarot_w04.png", "tarot_w05.png", "tarot_w06.png", "tarot_w07.png", "tarot_w08.png", "tarot_w09.png", "tarot_w10.png", "tarot_w11.png", "tarot_w12.png", "tarot_w13.png", "tarot_w14.png"],
+    "darkana": ["tarot_00.png", "tarot_01.png", "tarot_02.png", "tarot_03.png", "tarot_04.png", "tarot_05.png", "tarot_06.png", "tarot_07.png", "tarot_08.png", "tarot_09.png", "tarot_10.png", "tarot_11.png", "tarot_12.png", "tarot_13.png", "tarot_14.png", "tarot_15.png", "tarot_16.png", "tarot_17.png", "tarot_18.png", "tarot_19.png", "tarot_20.png", "tarot_21.png", "tarot_c01.png", "tarot_c02.png", "tarot_c03.png", "tarot_c04.png", "tarot_c05.png", "tarot_c06.png", "tarot_c07.png", "tarot_c08.png", "tarot_c09.png", "tarot_c10.png", "tarot_c11.png", "tarot_c12.png", "tarot_c13.png", "tarot_c14.png", "tarot_p01.png", "tarot_p02.png", "tarot_p03.png", "tarot_p04.png", "tarot_p05.png", "tarot_p06.png", "tarot_p07.png", "tarot_p08.png", "tarot_p09.png", "tarot_p10.png", "tarot_p11.png", "tarot_p12.png", "tarot_p13.png", "tarot_p14.png", "tarot_s01.png", "tarot_s02.png", "tarot_s03.png", "tarot_s04.png", "tarot_s05.png", "tarot_s06.png", "tarot_s07.png", "tarot_s08.png", "tarot_s09.png", "tarot_s10.png", "tarot_s11.png", "tarot_s12.png", "tarot_s13.png", "tarot_s14.png", "tarot_w01.png", "tarot_w02.png", "tarot_w03.png", "tarot_w04.png", "tarot_w05.png", "tarot_w06.png", "tarot_w07.png", "tarot_w08.png", "tarot_w09.png", "tarot_w10.png", "tarot_w11.png", "tarot_w12.png", "tarot_w13.png", "tarot_w14.png"],
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.querySelector("#tarotDecks").addEventListener('change', function() { 
+        // [2024-12-07-DA] save set
+        localStorage.setItem('set', this.value); 
+        // [2024-12-07-DA] set style of set
+        let themes = document.querySelectorAll("#theme");
+        for (let i = 0; i < themes.length; i++) themes[i].disabled = themes[i].href.indexOf(this.value) == -1 ? true : false;
+        // [2024-12-07-DA] load set
+        chooseSet(this.value); 
+    });
+
+    let s = document.querySelector("#tarotDecks").value;
+    if (!localStorage.getItem("set")) localStorage.setItem('set', s);
+    else {
+        s = localStorage.getItem('set');
+        document.querySelector("#tarotDecks").value = s;
+    }
+
+    // [2024-12-07-DA] load set
+    let eventChange = new Event('change');
+    document.querySelector("#tarotDecks").dispatchEvent(eventChange);
+});
+
+// var gothic = {
+//     0: {
+//         0: ["0. The Fool", "1. The Magician", "2. The High Priestess", "3. The Empress", "4. The Emperor", "5. The Hierophant", "6. The Lovers", "7. The Chariot", "8. Justice", "9. The Hermit", "10. Wheel of Fortune", "11. Strength", "12. The Hanged Man", "13. Death", "14. Temperance", "15. The Devil", "16. The Tower", "17. The Star", "18. The Moon", "19. The Sun", "20. Judgement", "21. The World"]
+//     },
+//     1: {
+//         0: ["Ace Wand", "Two Wands", "Three Wands", "Four Wands", "Five Wands", "Six Wands", " Seven Wands", "Eight Wands", "Nine Wands", "Ten Wands", "Knave Wands", "Knight Wands", "Queen Wands", "King Wands"],
+//         1: ["Ace Sword", "Two Swords", "Three Swords", "Four Swords", "Five Swords", "Six Swords", " Seven Swords", "Eight Swords", "Nine Swords", "Ten Swords", "Knave Swords", "Knight Swords", "Queen Swords", "King Swords"],
+//         2: ["Ace Cup", "Two Cups", "Three Cups", "Four Cups", "Five Cups", "Six Cups", " Seven Cups", "Eight Cups", "Nine Cups", "Ten Cups", "Knave Cups", "Knight Cups", "Queen Cups", "King Cups"],
+//         3: ["Ace Pentacle", "Two Pentacles", "Three Pentacles", "Four Pentacles", "Five Pentacles", "Six Pentacles", " Seven Pentacles", "Eight Pentacles", "Nine Pentacles", "Ten Pentacles", "Knave Pentacles", "Knight Pentacles", "Queen Pentacles", "King Pentacles"],
+//     }
+// }
+
+// [2023-02-10-DA] click to flip card
+function flip(that) {
+    var state = ['flip', 'flipFront'], isC = that.classList.contains('flip');
+    if (isC) {
+        getCard(that);
+        that.classList.replace(state[0], state[1]);
+        setTimeout(function () {that.querySelector('.card-back').style.display = 'none';}, 500);
+    }
+}
+
+// [2023-02-12-DA] pick a card and delete this card from array
+function getCard(that) {
+    // var s = document.querySelector("#tarotDecks").value;
+    // [2023-02-11-DA] mix decks
+    window.decks = shuffle(window.decks);
+    var nbrDecks = window.decks.length, pos = pickN(nbrDecks), deck = window.decks[pos] + "", rev = pickN(2), $card = that.querySelector(".card-front");
+    window.decks = window.decks.slice(0,pos).concat(window.decks.slice(pos+1));
+    $card.querySelector(`#${deck.split('.')[0]}`).style.transform = (rev) ? "scaleY(-1) scaleX(-1)" : "";
+    $card.querySelector(`#${deck.split('.')[0]}`).style.display = '';
+    // $card.innerHTML = `<img class="deck" src="./${s}/${deck}" style="${(rev) ? "transform: scaleY(-1) scaleX(-1);" : ""}">`;
+}
+
+function pickN(max) { return Math.floor(Math.random() * max); }
+
+// [2023-02-11-DA] reset all cards
+function reset() {
+    let s = document.querySelector("#tarotDecks").value, cards = document.querySelectorAll(".flipFront");
+    if (s == "") s = "gothic";
+    window.decks = set[s];
+    // [2023-02-11-DA] reset all choose cards 
+    document.querySelectorAll(".flip").forEach(function(ele) {ele.querySelector('.card-back').style.display = null});
+    for (let i = 0, card; (card = cards[i]); i++) {
+        card.querySelector('.card-back').style.display = null;
+        // setTimeout(function () {card.querySelector(".card-front").innerHTML = "";}, 1000);
+        setTimeout(function () {card.classList.replace("flipFront", "flip");}, 100);
+        card.querySelector(".card-front").querySelectorAll(".deck").forEach(function(ele) {setTimeout(function () {ele.style.display = "none";}, 500)});
+    }
+}
+
+// [2023-02-12-DA] shuffle
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function chooseSet(n) {
+    var name = n ? n : 'gothic';
+    window.decks = set[name];
+
+    // [2024-12-07-DA] set back of card
+    var $cards = document.querySelectorAll(".card-back");
+    for (let c = 0; c < $cards.length; c++) {
+        var $card = $cards[c];
+        $card.innerHTML = '';
+        var img = document.createElement('img');
+        img.className = 'deck';
+        img.src = `images/${name}/cardback.png`;
+        $card.appendChild(img);
+    }
+
+    // [2024-12-07-DA] reset front of card and load all cards
+    var $cards = document.querySelectorAll(".card-front"), setDeck = '';
+    for (let c = 0; c < $cards.length; c++) {
+        var $card = $cards[c];
+        $card.innerHTML = '';
+        for (var i = 0, l = window.decks.length; i<l; i++ ) {
+            var img = document.createElement('img');
+            img.className = 'deck';
+            img.src = `images/${name}/${window.decks[i]}`;
+            img.id = `${window.decks[i].split('.')[0]}`;
+            img.style.display = 'none';
+            $card.appendChild(img);
+        }
+    }
+
+    reset();
+}
