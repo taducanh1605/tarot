8f7c138a16757fd991ba05d630dbbe866abb4139
Duc Anh TA <83185055+taducanh1605@users.noreply.github.com>
2024-12-08 13:30:30 +0100

    update spreads

--- js/spread.js
+++ js/spread.js
@@ -4,7 +4,54 @@ var set = {
     "darkana": ["tarot_00.png", "tarot_01.png", "tarot_02.png", "tarot_03.png", "tarot_04.png", "tarot_05.png", "tarot_06.png", "tarot_07.png", "tarot_08.png", "tarot_09.png", "tarot_10.png", "tarot_11.png", "tarot_12.png", "tarot_13.png", "tarot_14.png", "tarot_15.png", "tarot_16.png", "tarot_17.png", "tarot_18.png", "tarot_19.png", "tarot_20.png", "tarot_21.png", "tarot_c01.png", "tarot_c02.png", "tarot_c03.png", "tarot_c04.png", "tarot_c05.png", "tarot_c06.png", "tarot_c07.png", "tarot_c08.png", "tarot_c09.png", "tarot_c10.png", "tarot_c11.png", "tarot_c12.png", "tarot_c13.png", "tarot_c14.png", "tarot_p01.png", "tarot_p02.png", "tarot_p03.png", "tarot_p04.png", "tarot_p05.png", "tarot_p06.png", "tarot_p07.png", "tarot_p08.png", "tarot_p09.png", "tarot_p10.png", "tarot_p11.png", "tarot_p12.png", "tarot_p13.png", "tarot_p14.png", "tarot_s01.png", "tarot_s02.png", "tarot_s03.png", "tarot_s04.png", "tarot_s05.png", "tarot_s06.png", "tarot_s07.png", "tarot_s08.png", "tarot_s09.png", "tarot_s10.png", "tarot_s11.png", "tarot_s12.png", "tarot_s13.png", "tarot_s14.png", "tarot_w01.png", "tarot_w02.png", "tarot_w03.png", "tarot_w04.png", "tarot_w05.png", "tarot_w06.png", "tarot_w07.png", "tarot_w08.png", "tarot_w09.png", "tarot_w10.png", "tarot_w11.png", "tarot_w12.png", "tarot_w13.png", "tarot_w14.png"],
 }
 
+var spreads = {
+    '1': [[1]],
+    '2': [[1, 1]],
+    '6': [[1, 1, 1, 1, 1, 1]],
+    'work': [  
+        [1, 0, 1, 0, 1],
+        [0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 0],
+    ],
+    'mirror': [
+        [0, 0, 1, 0, 0],
+        [0, 1, 0, 1, 0],
+        [1, 0, 0, 0, 1],
+        [0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 0],
+    ],
+    '3': [[1, 1, 1]],
+    '4': [[1, 1, 1, 1]],
+    '5': [[1, 1, 1, 1, 1]],
+}
+
+// [2024-12-07-DA] init
 document.addEventListener("DOMContentLoaded", function(event) {
+
+    // [2024-12-08-DA] load options for set and spread
+    let sel_set = document.querySelector("#tarotDecks");
+    for (let i = 0; i < Object.keys(set).length; i++) {
+        let opt = document.createElement("option");
+        opt.value = Object.keys(set)[i];
+        opt.innerHTML = lib(Object.keys(set)[i]);
+        if (i == 0) opt.selected = true;
+        sel_set.appendChild(opt);
+    }
+    let sel_spread = document.querySelector("#spreads");
+    for (let i = 0; i < Object.keys(spreads).length; i++) {
+        let opt = document.createElement("option");
+        opt.value = Object.keys(spreads)[i];
+        opt.innerHTML = lib(Object.keys(spreads)[i]);
+        if (i == 0) opt.selected = true;
+        sel_spread.appendChild(opt);
+    }
+
+    let eventChange = new Event('change');
+    
+    // -----------------------------------------------------------------------
+    // [2024-12-07-DA] for set
+    // -----------------------------------------------------------------------
+
     document.querySelector("#tarotDecks").addEventListener('change', function() { 
         // [2024-12-07-DA] save set
         localStorage.setItem('set', this.value); 
@@ -15,6 +62,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         chooseSet(this.value); 
     });
 
+    // [2024-12-07-DA] load selected set in localstorage
     let s = document.querySelector("#tarotDecks").value;
     if (!localStorage.getItem("set")) localStorage.setItem('set', s);
     else {
@@ -23,8 +71,32 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
 
     // [2024-12-07-DA] load set
-    let eventChange = new Event('change');
     document.querySelector("#tarotDecks").dispatchEvent(eventChange);
+
+    // -----------------------------------------------------------------------
+    // [2024-12-07-DA] for spread
+    // -----------------------------------------------------------------------
+
+    document.querySelector("#spreads").addEventListener('change', function() { 
+        // [2024-12-07-DA] save set
+        localStorage.setItem('spread', this.value); 
+        // [2024-12-07-DA] build spread
+        buildSpread(window.spreads[this.value]);
+        // [2024-12-07-DA] load set
+        chooseSet(localStorage.getItem("set")); 
+    });
+
+    // [2024-12-07-DA] load selected set in localstorage
+    let sp = document.querySelector("#spreads").value;
+    if (!localStorage.getItem("spread")) localStorage.setItem('spread', s);
+    else {
+        s = localStorage.getItem('spread');
+        document.querySelector("#spreads").value = sp;
+    }
+
+    // [2024-12-07-DA] load spread
+    document.querySelector("#spreads").dispatchEvent(eventChange);
+
 });
 
 // var gothic = {
@@ -61,6 +133,7 @@ function getCard(that) {
     // $card.innerHTML = `<img class="deck" src="./${s}/${deck}" style="${(rev) ? "transform: scaleY(-1) scaleX(-1);" : ""}">`;
 }
 
+// [2024-12-07-DA] pick random
 function pickN(max) { return Math.floor(Math.random() * max); }
 
 // [2023-02-11-DA] reset all cards
@@ -87,6 +160,7 @@ function shuffle(array) {
     return array;
 }
 
+// [2024-12-07-DA] load decks of set foreach card
 function chooseSet(n) {
     var name = n ? n : 'gothic';
     window.decks = set[name];
@@ -97,7 +171,7 @@ function chooseSet(n) {
         var $card = $cards[c];
         $card.innerHTML = '';
         var img = document.createElement('img');
-        img.className = 'deck';
+        img.className = 'card';
         img.src = `images/${name}/cardback.png`;
         $card.appendChild(img);
     }
@@ -109,7 +183,7 @@ function chooseSet(n) {
         $card.innerHTML = '';
         for (var i = 0, l = window.decks.length; i<l; i++ ) {
             var img = document.createElement('img');
-            img.className = 'deck';
+            img.className = 'card';
             img.src = `images/${name}/${window.decks[i]}`;
             img.id = `${window.decks[i].split('.')[0]}`;
             img.style.display = 'none';
@@ -119,3 +193,61 @@ function chooseSet(n) {
 
     reset();
 }
+
+// [2024-12-07-DA] make spread from [posx, posy]
+function buildSpread(spread) {
+    // [2024-12-08-DA] calculate size and position
+    let row_len = spread.length, col_len = spread[0].length;
+    let h = `${(70 / row_len).toFixed(2)}vh`, w = `${(99 / col_len).toFixed(2)}vw`;
+
+    // [2024-12-08-DA] clear spread
+    let $spread = document.querySelector(".table-spread");
+    $spread.innerHTML = '';
+
+    for (let i = 0; i < row_len; i++) {
+        let row = document.createElement('div');
+        row.className = 'row-spread';
+        for (let j = 0; j < col_len; j++) {
+            // [2024-12-08-DA] make a card
+            let card = document.createElement('div');
+            card.className = 'space';
+            if (spread[i][j]) {
+                card.className = 'flip space';
+                card.id = `card${i}_${j}`;
+                card.setAttribute("onClick", "flip(this)");
+                // [2024-12-08-DA] back of card
+                let card_back = document.createElement('div');
+                card_back.className = 'card-back';
+                let img = document.createElement('img');
+                img.className = 'card';
+                img.src = `images/${localStorage.getItem("set")}/cardback.png`;
+                card_back.appendChild(img);
+                // [2024-12-08-DA] front of card
+                let card_front = document.createElement('div');
+                card_front.className = 'card-front';
+                card_front.innerHTML = '';
+                // [2024-12-08-DA] add both of them to card
+                card.appendChild(card_back);
+                card.appendChild(card_front);
+            }
+            row.appendChild(card);
+        }
+        $spread.appendChild(row);
+    }
+
+    // [2024-12-08-DA] load set
+    chooseSet(localStorage.getItem("set"));
+
+    // [2024-12-08-DA] set size and position
+    let cards = document.querySelectorAll(".space");
+    for (let i = 0, card; (card = cards[i]); i++) {
+        card.style.width = w;
+        card.style.height = h;
+    }
+}
+
+var lib = function (s) { 
+    let tmp = s.replaceAll('-', ' ').replaceAll('_', ' ').split(' ');
+    for (let i = 0; i < tmp.length; i++) tmp[i] = tmp[i].charAt(0).toUpperCase() + tmp[i].slice(1);
+    return tmp.join(' ');
+};
